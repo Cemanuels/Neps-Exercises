33d4278ffc1f53aa4a7817234a74eaeffa25294d
Cemanuels <c.emanuel_ufc@outlook.com>
2019-05-22 23:56:52 -0300

    Update Ex1.py
    first change

--- Ex1.py
+++ Ex1.py
@@ -1,1 +1,14 @@
-print('Teste em python!')
+t = int(input())
+result = []
+for i in range(t):
+	n = int(input())
+	s = input()
+	for j in range(n):
+		if s[j] == '8' and len(s) - (j+1) >= 11:
+			result.append('YES')	
+			break
+		elif len(s) - (j+1) < 11:
+			result.append('NO')
+			break
+for i in result:
+	print(i)
