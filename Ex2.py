3d5aca7a2de803de8a7eb06f7cb7308cc025df7d
Cemanuels <c.emanuel_ufc@outlook.com>
2019-05-24 09:54:45 -0300

    Create Ex2.py
    Sum of matrix 3x3

---
+++ Ex2.py
@@ -0,0 +1,11 @@
+matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+sum_colun = [0, 0, 0]
+for i in range(3):
+	for j in range(3):
+		n = int(input())
+		matrix[i][j] = n
+for colun in  range(3):
+	for line in range(3):
+		sum_colun[colun] += matrix[line][colun]
+for i in range(3):
+	print(f'Coluna {i}: {sum_colun[i]}')
